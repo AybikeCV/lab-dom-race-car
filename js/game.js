69cb2a3177b6868c347b5b0d971b547a9d88f828
Aybike Celebi Visser <celebiaybike@gmail.com>
2026-04-10 19:16:33 +0200

    tried to solve, keys not working

--- js/game.js
+++ js/game.js
@@ -1,3 +1,95 @@
 class Game {
-    // code to be added
+
+    constructor () {
+        this.startScreen = document.querySelector("#game-intro");
+        this.gameScreen = document.querySelector("#game-screen");
+        this.gameEndScreen = document.querySelector("#game-end");
+        this.player = null;
+        this.height = 600;
+        this.width = 500;
+        this.obstacles = [];
+        this.score = 0;
+        this.lives = 3;
+        this.gameIsOver = false;
+        this.gameIntervalId;
+        this.gameLoopFrequency = Math.round(1000/60);
+
+        this.player = new Player(
+            this.gameScreen,
+            200,
+            500,
+            100,
+            150,
+            "./images/car.png"
+    );
+    }
+
+     start() {
+    // Set the height and width of the game screen
+    this.gameScreen.style.height = `${this.height}px`;
+    this.gameScreen.style.width = `${this.width}px`;
+
+    // Hide the start screen
+    this.startScreen.style.display = "none";
+    
+    // Show the game screen
+    this.gameScreen.style.display = "block";
+
+    // Runs the gameLoop on a fequency of 60 times per second. Also stores the ID of the interval.
+    this.gameIntervalId = setInterval(() => this.gameLoop(), this.gameLoopFrequency) 
+     }
+
+  gameLoop() {
+    
+    this.update();
+
+    // If "gameIsOver" is set to "true" clear the interval to stop the loop
+    if (this.gameIsOver) clearInterval(this.gameIntervalId)
+    
+  }
+
+  update() {
+    
+    this.player.move();
+
+     for (let i = 0; i < this.obstacles.length; i++) {
+      const obstacle = this.obstacles[i];
+      obstacle.move();
+
+      if (this.player.didCollide(obstacle)) {
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        this.lives--;
+        i--;
+        }
+        else if (obstacle.top > this.height) {
+        this.score++;
+        obstacle.element.remove();
+        this.obstacles.splice(i, 1);
+        i--;
+        }
+      }
+
+      if (this.lives === 0) {
+        this.endGame();
+      }
+
+      if (Math.random() > 0.98 && this.obstacles.length < 1) {
+      this.obstacles.push(new Obstacle(this.gameScreen));
+    }
 }
+    endGame() {
+      this.player.element.remove();
+      
+      this.obstacles.forEach(obstacle => obstacle.element.remove());
+     
+      this.gameIsOver = true;
+      
+      this.gameScreen.style.display = "none";
+      
+      this.gameEndScreen.style.display = "block";
+      
+  }
+}
+
+
--- js/script.js
+++ js/script.js
@@ -1,12 +1,84 @@
+
 window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
+  const game = new Game("game-intro", "game-screen", "game-end");
 
   startButton.addEventListener("click", function () {
     startGame();
   });
+  
+  restartButton.addEventListener("click", function () {
+    restartGame();
+  });
 
   function startGame() {
-    console.log("start game");
+    game.start();
   }
+  function restartGame() {
+    location.reload();
+  }
+
+  function handleKeydown(event) {
+      if (event.type === "keydown") {
+        switch (event.key) {
+          case "ArrowLeft":
+            game.player.directionX = -1;
+            break;
+          case "ArrowUp":
+            game.player.directionY = -1;
+            break;
+          case "ArrowRight":
+            game.player.directionX = 1;
+            break;
+          case "ArrowDown":
+            game.player.directionY = 1;
+            break;
+        }
+      }
+        else if (event.type === "keyup") {
+          switch (event.key) {
+            case "ArrowLeft":
+              game.player.directionX = 0;
+              break;
+            case "ArrowUp":
+              game.player.directionY = 0;
+              break;
+            case "ArrowRight":
+              game.player.directionX = 0;
+              break;
+            case "ArrowDown":
+              game.player.directionY = 0;
+              break;
+          }
+        }
+      }
+
+
+  window.addEventListener("keydown", handleKeydown);
+  window.addEventListener("keyup", handleKeydown);
+
+
+
+
 };
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
